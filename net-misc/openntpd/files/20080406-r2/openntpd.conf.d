ea28a0bc63c1b1d5c880e488a57881114c909776
Bruce Guenter <bruce@untroubled.org>
2013-08-05 15:07:00 -0600

    net-misc/openntpd: Add ebuild for version 20080406-r2
    The new package no longer needs any adjtimex patching, as the
    functionality is provided through a compat hook.

---
+++ net-misc/openntpd/files/20080406-r2/openntpd.conf.d
@@ -0,0 +1,9 @@
+# /etc/conf.d/ntpd: config file for openntpd's ntpd
+
+NTPD_HOME=/var/lib/openntpd/chroot
+
+# See ntpd(8) man page ... some popular options:
+#  -s   Set the time immediately at startup
+NTPD_OPTS="-s"
+
+MULTILOG_OPTS="-'* reply from *'"
